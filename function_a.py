f7239ac0ca0cd74817ab8edffa8f3bdd872cfef2
HaileyMatz <hmatz525@gmail.com>
2022-03-25 13:42:33 -0700

    from Hailey

--- function_a.py
+++ function_a.py
@@ -16,6 +16,7 @@ def most_common_value(number_list):
 
     return most_common_value
 
+print("Hailey wrote this, this is fun! Nice to work with you all :)")
 
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
